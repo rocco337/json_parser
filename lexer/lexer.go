9424702d388797fe721c6bb51b7ef424d252b208
rocco337 <roko.bobic@gmail.com>
2019-12-13 16:17:18 +0100

    create basic lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,8 +1,97 @@
 package lexer
 
+import (
+	"errors"
+	"strconv"
+
+	"github.com/thoas/go-funk"
+)
+
 //Lex - Parse json and return tokens
-func Lex(input string) []string {
-	tokens := make([]string, 0)
-	tokens = append(tokens, input)
+//Find patterns of strings, numbers, booleans, nulls, or JSON syntax like left brackets and left braces,
+func Lex(input string) []interface{} {
+	tokens := lexRecursive(input, 0, "")
 	return tokens
+}
+
+var openChars = []string{"{", "\"", "["}
+var closeChars = []string{"}", "\"", "]"}
+var ignoreChars = []string{":", ","}
+
+func lexRecursive(input string, position int, tempValue string) []interface{} {
+	result := make([]interface{}, 0)
+	if len(input) <= 0 || position > len(input)-1 {
+		return result
+	}
+
+	//more aobut code points on https://blog.golang.org/strings
+	codePoint := string(input[position])
+
+	if funk.Contains(ignoreChars, codePoint) {
+		//move on
+	} else if funk.Contains(openChars, codePoint) || funk.Contains(closeChars, codePoint) {
+		if len(tempValue) > 0 {
+			result = append(result, parseValue(tempValue)...)
+			tempValue = ""
+		}
+		if codePoint != "\"" {
+			result = append(result, codePoint)
+		}
+	} else {
+		tempValue += codePoint
+	}
+
+	position++
+	result = append(result, lexRecursive(input, position, tempValue)...)
+
+	return result
+}
+
+func parseValue(input string) []interface{} {
+	result := make([]interface{}, 0)
+
+	isNull := isNull(input)
+	//check if value is null
+	if isNull {
+		result = append(result, nil)
+	} else {
+		//check if value is integer
+		intValue, err := parseInt(input)
+		if err != nil {
+			boolValue, err := parseBool(input)
+			if err == nil {
+				//is nill??
+				result = append(result, boolValue)
+			} else {
+				//if value couldnt be parsed as any of specific parsers than it is a string
+				result = append(result, input)
+			}
+		} else {
+			result = append(result, intValue)
+		}
+	}
+
+	return result
+}
+
+func parseInt(input string) (uint64, error) {
+	parsed, err := strconv.ParseUint(input, 10, 64)
+	return parsed, err
+}
+
+func parseBool(input string) (bool, error) {
+	if input == "true" {
+		return true, nil
+	} else if input == "false" {
+		return false, nil
+	}
+
+	return false, errors.New("Cannot parse as bool")
+}
+
+func isNull(input string) bool {
+	if input == "null" {
+		return true
+	}
+	return false
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,13 +1,55 @@
 package lexer
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 func TestLexer(t *testing.T) {
 
 	t.Run("Lex - should return tokens", func(t *testing.T) {
-		tokens := Lex("{ \"userId\": 1}")
-		if tokens[0] != "{" {
-			t.Fail()
+		user := struct {
+			Userid              int
+			Roles               []string
+			IsActive            bool
+			ShouldResetPassword bool
+			ParentUserID        string
+		}{
+			1,
+			[]string{"Admin", "User", "Operator"},
+			true,
+			false,
+			"null",
+		}
+		userJSON, err := json.Marshal(user)
+		if err != nil {
+			t.Fatalf("Cannot serialize test data")
+		}
+		tokens := Lex(string(userJSON))
+
+		expectedResult := []string{
+			"{",
+			"Userid", "1",
+			"Roles", "[",
+			"Admin", "User", "Operator",
+			"]",
+			"IsActive", "true",
+			"ShouldResetPassword", "false",
+			"ParentUserID", "null",
+			"}",
+		}
+
+		if len(expectedResult) != len(tokens) {
+			t.Errorf("Result has different length than expected result. Expected: %+q Got: %+q", expectedResult, tokens)
+		}
+
+		for i := 0; i < len(expectedResult); i++ {
+			if expectedResult[i] == "null" && tokens[i] == nil {
+				//its ok, move on
+			} else if expectedResult[i] != fmt.Sprintf("%v", tokens[i]) {
+				t.Errorf("Values differ: Expected: %s Got: %v", expectedResult[i], tokens[i])
+			}
 		}
 	})
 
