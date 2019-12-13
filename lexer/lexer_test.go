47e99b10bc7a2bdbaa1a1ed4ae6a5a72db7ce1f6
rocco337 <roko.bobic@gmail.com>
2019-12-13 12:39:32 +0100

    add colored test runner

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestLexer(t *testing.T) {
 
-	t.Run("Lex should return tokens", func(t *testing.T) {
+	t.Run("Lex - should return tokens", func(t *testing.T) {
 		tokens := Lex("{ \"userId\": 1}")
 		if tokens[0] != "{" {
 			t.Fail()
